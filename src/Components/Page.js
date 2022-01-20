e7f66ef3e498cd21e9965b5faee768c560233343
Youssef Mohamed <48146406+youssefMoJo@users.noreply.github.com>
2022-01-19 23:45:54 -0330

    Add files via upload

---
+++ src/Components/Page.js
@@ -0,0 +1,131 @@
+import React from 'react';
+import PostCard from "./PostCard"
+import styled from "styled-components";
+import DatePicker from 'react-date-picker';
+
+const axios = require('axios');
+
+const DateContainer = styled.div`
+   background-color: white;
+   border-radius: 10px;
+   width: 50%;
+   margin-left: 25%;
+   position: relative;
+`;
+
+class Page extends React.Component {
+
+    state = {
+        fromDate: new Date(),
+        lastFormatedDate: new Date(),
+        data: null
+    }
+
+    formateDate = (date) => {
+
+        let formatedDate = date.toISOString().split('T')[0]
+
+        let current = date
+        let followingTwoDays = new Date(current.getTime() + 172800000).toLocaleDateString().split("/")
+        let nextTwoDaysDate = `${followingTwoDays[2]}-${followingTwoDays[1]}-${followingTwoDays[0]}`
+
+
+        this.setState({ lastFormatedDate: formatedDate, fromDate: date }, () => {
+            axios.get(`https://api.nasa.gov/planetary/apod?api_key=HCNJlv1NpUkMhXdycaSVluE5RHY4GQcYwdz5GU5E&start_date=${formatedDate}&end_date=${nextTwoDaysDate}`).then(response => {
+                this.setState({ data: response.data })
+            }).catch(function (error) {
+                console.log(error);
+            })
+        })
+
+    }
+
+    componentDidMount() {
+        var d = new Date(),
+            month = '' + (d.getMonth() + 1),
+            day = '' + d.getDate(),
+            year = d.getFullYear();
+
+        if (month.length < 2)
+            month = '0' + month;
+        if (day.length < 2)
+            day = '0' + day;
+
+        let formatedDate = [year, month, day].join('-');
+
+
+        axios.get(`https://api.nasa.gov/planetary/apod?api_key=HCNJlv1NpUkMhXdycaSVluE5RHY4GQcYwdz5GU5E&start_date=${formatedDate}`).then(response => {
+            this.setState({ data: response.data })
+        }).catch(function (error) {
+            console.log(error);
+        })
+    }
+    getCards = () => {
+        let allCards = []
+        if (this.state.data.length === 1) {
+            allCards.push(
+                <PostCard
+                    titles={this.state.data[0].title}
+                    date={this.state.data[0].date}
+                    explanation={this.state.data[0].explanation}
+                    url={this.state.data[0].url}
+                    key={0}
+                />
+            )
+        } else if (this.state.data.length === 2) {
+            for (let i = 0; i < 2; i++) {
+                allCards.push(
+                    <PostCard
+                        titles={this.state.data[i].title}
+                        date={this.state.data[i].date}
+                        explanation={this.state.data[i].explanation}
+                        url={this.state.data[i].url}
+                        key={i}
+                    />
+                )
+            }
+        } else {
+
+            for (let i = 0; i <= 2; i++) {
+                allCards.push(
+                    <PostCard
+                        titles={this.state.data[i].title}
+                        date={this.state.data[i].date}
+                        explanation={this.state.data[i].explanation}
+                        url={this.state.data[i].url}
+                        key={i}
+                    />
+                )
+            }
+        }
+        return allCards
+    }
+    render() {
+
+
+        return (
+            <div>
+                {this.state.data === null ? console.log("") :
+                    < div >
+                        <DateContainer>
+                            From
+                            <DatePicker
+                                onChange={(date) => {
+                                    this.formateDate(date)
+                                }}
+                                value={this.state.fromDate}
+                            />
+                            Up To Three Days
+                        </DateContainer>
+                        {this.getCards()}
+                    </div>
+                }
+
+            </div >
+        )
+    }
+
+
+}
+
+export default Page;
---
+++ src/Components/PostCard.js
@@ -0,0 +1,81 @@
+import React from "react";
+import styled from "styled-components";
+
+const MainContainer = styled.div`
+  position: relative;
+  width: fit-content;
+  margin: 15px;
+  float: left;
+  height: 850px;
+  width: 590px;
+  border-radius: 10px;
+  background-color: white;
+  margin-left: 23px;
+`;
+
+const PostPic = styled.img`
+  height: 300px;
+  box-shadow: 0 2px 5px #000;
+  width: 100%;
+  border-radius: 7px;
+  object-fit: cover;
+`;
+const PostTitle = styled.div`
+    margin: 10px;
+    font-family: Serif;
+    width: 100%;
+    font-size: 25px;
+    font-weight: 700;
+    text-align: left;
+`;
+const PostExplanation = styled.div`
+    margin: 10px;
+    font-family: Arial;
+    width: 97%;
+    font-size: 20px;
+    text-align: left;
+    height: 400px;
+    overflow-y: scroll;
+`;
+const LikeButton = styled.button`
+    background-color: green;
+    color: white;
+    font-size: 25px;
+`;
+
+class PostCard extends React.Component {
+    render() {
+        return (
+            <MainContainer >
+                <PostPic
+                    src={this.props.url}
+                />
+                <PostTitle>
+                    {this.props.titles}
+                    <div style={{
+                        fontSize: "20px",
+                        fontWeight: "0",
+                        fontFamily: "Monospace",
+                        marginTop: "5px",
+                        color: "rgb(57, 24, 247)"
+                    }}>
+                        {this.props.date}
+                    </div>
+                </PostTitle>
+                <PostExplanation>
+                    {this.props.explanation}
+                </PostExplanation>
+
+                {/* I was going to make an object in the local storage to add the liked posts or pictures for that user and 
+                    once the user reloads the page, I will look in the object if any of the three posts in the scereen is liked by 
+                    the user.
+                */}
+                <LikeButton >
+                    Like
+                </LikeButton>
+            </MainContainer>
+        );
+    }
+}
+
+export default PostCard;
